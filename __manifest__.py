207a9815297195dc35e562a744e3b7d0b2a990d3
evrardachi <achi.a.evrard@gmail.com>
2026-01-04 21:24:51 +0000

    Update Message de masse

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Afruxia SMS',
-    'version': '19.0.1.0.2',
+    'version': '19.0.1.0.3',
     'category': 'Tools',
     'summary': 'Envoi de SMS via l\'API Afruxia',
     'description': """
